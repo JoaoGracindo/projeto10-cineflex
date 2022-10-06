1377d1711189a06fd620b72b8cbbe81eed6d3c22
JoaoGracindo <gabrielpessoa97@gmail.com.com>
2022-10-06 13:31:46 -0300

    header component

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export default function Header(){
+    return(
+        <Topo>
+            Cineflix
+        </Topo>
+    )
+}
+
+const Topo = styled.header`
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 67px;
+    background-color:#C3CFD9;
+    font-family: 'Roboto';
+    font-size: 34px;
+    font-weight:400;
+    display: flex;
+    align-items: center;
+    justify-content:center;
+    color: #E8833A;
+
+`
