835a7902f482026b8001dc21d70721b93699ad36
JoaoGracindo <gabrielpessoa97@gmail.com.com>
2022-10-10 00:28:05 -0300

    selection and post

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import Header from "./Header";
 import Catalogo from "./Catalogo";
 import Sessoes from "./Sessoes";
+import Assentos from "./Assentos";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
@@ -12,6 +13,7 @@ export default function App(){
             <Routes>
                 <Route path="/" element={<Catalogo/>}/>
                 <Route path="/sessoes/:idFilme" element={<Sessoes/>}/>
+                <Route path="/assentos/:idSessao" element={<Assentos/>}/>
             </Routes>
 
         </BrowserRouter>
---
+++ src/components/Assentos.js
@@ -0,0 +1,172 @@
+import styled from "styled-components";
+import { useParams, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function Assentos() {
+  const { idSessao } = useParams();
+  const [seats, setSeats] = useState([]);
+  const [movie, setMovie] = useState({});
+  const [nome, setNome] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [ids,setIds] = useState([]);
+  let arrayId = [];
+
+  function Seats({name, id, selected, isAvailable}){
+
+    return(
+      <Assento onClick={() => selecionar(id)} isAvailable={isAvailable} selected={selected}>
+        {name}
+      </Assento>
+    )
+  }
+  useEffect(() => {
+    axios
+      .get(
+        `https://mock-api.driven.com.br/api/v5/cineflex/showtimes/${idSessao}/seats`
+      )
+      .then((response) => {
+        setSeats([...response.data.seats]);
+        setMovie(response.data.movie);
+      });
+  }, []);
+
+  function reservarAssentos(e){
+    e.preventDefault();
+    console.log(nome,cpf)
+    const body = {
+      ids: arrayId,
+      nome:nome,
+      cpf:cpf
+    };
+    axios.post("https://mock-api.driven.com.br/api/v5/cineflex/seats/book-many",body )
+    .then(() => {console.log("foi")})
+  }
+
+  function selecionar(id){
+      if(ids.includes(id)){
+        setIds(ids.filter((i) => i != id))
+        arrayId = ids.filter((i) => i != id)
+      }else{
+        setIds([...ids,id])
+        arrayId = [...ids,id]
+  } 
+  console.log(arrayId)
+}
+
+  if (!seats) {
+    return "";
+  }
+  return (
+    <>
+      <Main>
+        <SessionTitle>Selecione o(s) assento(s)</SessionTitle>
+        <SeatsContainer>
+          {seats.map((seat) => (
+            <Seats name={seat.name} selected={!ids.includes(seat.id)} id={seat.id} isAvailable={seat.isAvailable}/>
+          ))}
+        </SeatsContainer>
+        <form onSubmit={reservarAssentos}>
+          <label forHTML="nome">nome</label>
+          <input onChange={(e) => setNome(e.target.value)} id="nome" name="nome" type="text" required/>
+          <label forHTML="cpf">cpf</label>
+          <input onChange={(e) => setCpf(e.target.value)} id="cpf" name="cpf" type="cpf" required/>
+          <button type="submit">Reservar Assentos</button>
+        </form>
+      </Main>
+      <Footer>
+        <img src={movie.posterURL} />
+        <span>{movie.title}</span>
+      </Footer>
+    </>
+  );
+}
+
+const SeatsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+const Assento = styled.div`
+  box-sizing: border-box;
+  width: 26px;
+  height: 26px;
+  background-color: ${(props) => {
+  if(props.isAvailable){
+    if(props.selected ){
+      return "#C3CFD9"
+    }else{
+      return "#1AAE9E"
+    }
+  }
+  return "#FBE192"
+}};
+  
+  border: 1px solid #808f9d;
+  border-radius: 12px;
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 11px;
+  line-height: 13px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  letter-spacing: 0.04em;
+  margin-right: 6px;
+  margin-bottom: 22px;
+`;
+
+const Footer = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 110px;
+  left: 0px;
+  bottom: 0px;
+  background-color: #dfe6ed;
+  border: 1px solid #9eadba;
+  display: flex;
+  align-items: center;
+
+  & img {
+    box-sizing: border-box;
+    height: 70px;
+    width: 64px;
+    border: 8px solid #ffffff;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+    border-radius: 2px;
+    margin-left: 10px;
+    margin-right: 15px;
+  }
+
+  & span {
+    font-family: "Roboto";
+    font-weight: 400;
+    font-size: 26px;
+    line-height: 30px;
+    color: #293845;
+  }
+`;
+const SessionTitle = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  letter-spacing: 0.04em;
+  color: #293845;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 28px;
+  height: 110px;
+`;
+
+const Main = styled.div`
+  position: fixed;
+  overflow-y: scroll;
+  top: 70px;
+  margin-left: 5vw;
+  margin-right: 5vw;
+  width: 90vw;
+  bottom: 120px;
+`
